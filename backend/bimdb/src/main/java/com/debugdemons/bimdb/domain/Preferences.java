ea2801ae04b26fb1479a8efcf7ebcf0e1c0960ca
Cristina Dias Pinto <diaspcri@students.zhaw.ch>
2023-05-23 08:50:16 +0200

    92 - adding Serializable import

--- backend/bimdb/src/main/java/com/debugdemons/bimdb/domain/Preferences.java
+++ backend/bimdb/src/main/java/com/debugdemons/bimdb/domain/Preferences.java
@@ -2,6 +2,7 @@ package com.debugdemons.bimdb.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class Preferences implements Serializable {
